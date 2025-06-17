ba6993812662f7114026aa115fa6ef23f0024089
eamsar <asmaeelmakhroubi@gmail.com>
2025-06-17 14:37:27 +0100

    first commit

---
+++ src/main/java/com/atliontech/website/AtliontechApplication.java
@@ -0,0 +1,13 @@
+package com.atliontech.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtliontechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtliontechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atliontech/website/AtliontechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atliontech.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtliontechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
